186fb7eab92218d1653a7584fffacea54d96f124
martin cardozo <tiin.-@hotmail.es>
2025-06-17 09:26:38 -0300

    se modifica el tunel de lo cors

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -70,7 +70,8 @@ class Server {
                     'http://localhost:5173',
                     'http://127.0.0.1:5173',
                     'http://localhost:3000',
-                    'http://127.0.0.1:3000'
+                    'http://127.0.0.1:3000',
+                    'https://vhbpfn9n-5173.brs.devtunnels.ms'
                 ],
                 methods: ['GET', 'POST'],
                 credentials: true
@@ -92,7 +93,8 @@ class Server {
                 'http://localhost:5173',
                 'http://127.0.0.1:5173',
                 'http://localhost:3000',
-                'http://127.0.0.1:3000'
+                'http://127.0.0.1:3000',
+                'https://vhbpfn9n-5173.brs.devtunnels.ms'
             ],
             methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
             credentials: true,
